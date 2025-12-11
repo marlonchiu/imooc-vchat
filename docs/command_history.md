77c03804746897a1d52f559dae7810b569666552
marlonchiu <Knight1898@outlook.com>
2025-12-11 21:23:52 +0800

    ✨ feat: 6-3 添加 vue3 支持

---
+++ docs/command_history.md
@@ -0,0 +1,9 @@
+## 操作命令历史
+
+```bash
+npx create-electron-app@latest vchat --template=vite-typescript
+
+npm install vue@latest
+
+npm install --save-dev @vitejs/plugin-vue@5.2.4 # 不要使用6.0.0 无法启动项目
+```
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,11 @@
  * ```
  */
 
-import './index.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+createApp(App).mount('#app')
 
 console.log(
   '👋 This message is being logged by "renderer.ts", included via Vite',
