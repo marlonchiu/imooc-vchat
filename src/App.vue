e8a9ca19b1cfbb1d818dec329aa59ee0b577424d
marlonchiu <Knight1898@outlook.com>
2025-12-12 14:10:53 +0800

    ✨ feat: 6-9 ProviderList第二部分

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,10 @@
         </button>
       </div>
     </div>
-    <div class="h-full flex-1">
-      <ProviderSelect :items="providers" />
+    <div class="h-full flex-1 flex items-center">
+      <div class="w-[80%] mx-auto">
+        <ProviderSelect :items="providers" />
+      </div>
     </div>
   </div>
 </template>
@@ -72,7 +74,8 @@ const providers: ProviderProps[] = [
     name: '文心一言',
     desc: '文心一言 百度出品的大模型',
     models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'],
-    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    // avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    avatar: 'https://img.alicdn.com/imgextra/i4/O1CN01Slt9qq1Q0CmLAG9MK_!!6000000001913-55-tps-32-32.svg',
     createdAt: '2024-07-03',
     updatedAt: '2024-07-03'
   },
@@ -82,7 +85,8 @@ const providers: ProviderProps[] = [
     desc: '通义千问',
     // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.0.5bf41507xgULX5#b148acc634pfc
     models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
-    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    // avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    avatar: 'https://img.alicdn.com/imgextra/i4/O1CN01Slt9qq1Q0CmLAG9MK_!!6000000001913-55-tps-32-32.svg',
     createdAt: '2024-07-03',
     updatedAt: '2024-07-03'
   }
