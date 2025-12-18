dd13eea25c2b58eb73c3bcd414970dff7d8d5fe5
marlonchiu <Knight1898@outlook.com>
2025-12-18 16:29:32 +0800

    ✨ feat: 11-15 重构通用模型 - 添加通用数据处理

--- src/providers/BaseProvider.ts
+++ src/providers/BaseProvider.ts
@@ -1,5 +1,6 @@
-import { ChatMessageProps } from '../types'
+import { ChatMessageProps, UniversalChunkProps } from '../types'
 
 export abstract class BaseProvider {
   abstract chat(messages: ChatMessageProps[], modelName: string): Promise<any>
+  protected abstract transformResponse(chunk: any): UniversalChunkProps
 }
--- src/types.ts
+++ src/types.ts
@@ -58,3 +58,13 @@ export type OnUpdatedCallback = (data: UpdatedStreamData) => void
 export interface MessageListInstance {
   ref: HTMLDivElement
 }
+
+export interface UniversalChunkProps {
+  is_end: boolean
+  result: string
+}
+
+export interface BaiduChunkProps {
+  is_end: boolean
+  result: string
+}
